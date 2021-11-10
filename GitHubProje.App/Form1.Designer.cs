b39e5fa3663a0d3b25b1848c0a87cff69ea627d3
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2021-11-10 11:43:00 +0300

    Lib projesi eklendi

--- GitHubProje.App/Form1.Designer.cs
+++ GitHubProje.App/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(446, 204);
+            this.button1.Location = new System.Drawing.Point(65, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(199, 138);
             this.button1.TabIndex = 0;
---
+++ GitHubProje.Lib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GitHubProje.Lib
+{
+    public class Class1
+    {
+    }
+}
